253b95252a7acf4d714ceb3e93d585f47da471ff
Mohsin Shafiq <mohsinshafiq@Mohsins-MacBook-Air.local>
2024-10-02 23:57:37 -0400

    Website push

---
+++ Website/database.php
@@ -0,0 +1,18 @@
+<?php
+ function getDB() {
+    $host = 'sql1.njit.edu';
+    $port = 3306;
+    $dbname = 'ms3472';
+    $username = 'ms3472';
+    $password = 'It2021234567!';
+   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+   try {
+       $db = new mysqli($host, $username, $password, $dbname, $port);
+       return $db;
+   } catch (mysqli_sql_exception $e) {
+       error_log($e->getMessage(), 0);
+       echo $e->getMessage();
+   }
+ }
+ getDB();
+?>
---
+++ Website/index.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Inventroy helper login</title></head>
+<body>
+    <h1> Welcome to the Portable Powerbank Inventroy website </h1>
+    <section id = "container">
+        <main>
+            <?php
+            if(isset($_REQUEST['content'])){
+                include($_REQUEST['content'] .'.inc.php');
+            }
+            else {
+                include ('main.inc.php');
+            }
+            ?>
+        </main>
+
+    </section>
+</body>
+</html>
